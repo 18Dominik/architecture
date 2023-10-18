4cc54416fe7a43b2fccbbfece71acc4af37e651f
Dominik <dominiksteffan92@gmail.com>
2023-10-18 10:11:22 +0000

    Update

--- Coupling_Delegation.py
+++ Coupling_Delegation.py
@@ -1,6 +1,7 @@
 # A usage or delegation is a method or function call that originiates in one component and is handled by another
 
 ## 1. Very bad: Component 'Car' uses some interal details of Component 'Engine'. If these details of 'Engine' are ever modified, then 'Car' is likely to break
+# for simplicity, we skip adding a superclass 'EngineType' and inherit start() method to subclasses Engine and ElektricMotor 
 
 
 class Engine:
@@ -9,7 +10,7 @@ class Engine:
     
 class ElectricMotor:
     def start(self):
-        print("Electric Motor starts")
+        print("Electric motor starts")
     
 class Car:
     '''
@@ -35,11 +36,15 @@ my_car.start()
 ###########
 
 ## 2: Better: Dependency Injection (DI) allows you to inject a specific engine into the Car class, reducing the coupling and making it easier to replace or extend components without modifying the Car class.
-
+# for simplicity, we skip adding a superclass 'EngineType' and inherit start() method to subclasses Engine and ElektricMotor 
 
 class Engine1:
     def start(self):
         print("Engine starts")
+
+class ElectricMotor1:
+    def start(self):
+        print("Electric motor starts")        
 
 class Car1:
     def __init__(self, engine): # Instead of tightly coupling the Car class with the Engine class, we use Dependency Injection. The Car class's constructor takes an instance of the Engine class as a parameter.
@@ -51,7 +56,7 @@ class Car1:
 
 # Usage
 ## Creating an Engine instance
-car_engine = Engine1() # We create an Engine instance separately and inject it into the Car instance when we create the Car.
+car_engine = Engine1() # We create an Engine instance separately and inject it into the Car instance when we create the Car. # as an alternative, use 'car_engine=ElectricMotor1()'
 
 ## Creating a Car instance and injecting the Engine instance
 my_car = Car1(car_engine)
